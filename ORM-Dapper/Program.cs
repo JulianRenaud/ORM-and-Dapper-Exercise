b8a744e8451bb930a01d9ae4aa428d237f6a3956
JulianRenaud <midway6220@gmail.com>
2022-10-11 10:17:38 -0500

    Finished E2 and bonus

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -12,15 +12,53 @@ string connString = config.GetConnectionString("DefaultConnection");
 
 IDbConnection conn = new MySqlConnection(connString);
 
-var departmentRepo = new DapperDepartmentRepository(conn);
+#region Department
 
-//departmentRepo.InsertDepartment("Anything");
+//var departmentRepo = new DapperDepartmentRepository(conn);
 
-var departments = departmentRepo.GetAllDepartments();
+////departmentRepo.InsertDepartment("Anything");
 
-foreach (var department in departments)
+//var departments = departmentRepo.GetAllDepartments();
+
+//foreach (var department in departments)
+//{
+//    Console.WriteLine($"{department.DepartmentID} {department.Name}");
+//    Console.WriteLine();
+//    Console.WriteLine();
+//}
+
+#endregion
+
+
+#region Product
+var productRepository = new DapperProductRepository(conn);
+
+//940
+
+var productToUpdate = productRepository.GetProduct(940);
+
+
+productToUpdate.Name = "Macbook";
+productToUpdate.Price = 1000;
+productToUpdate.CategoryID = 1;
+productToUpdate.StockLevel = 866;
+productToUpdate.OnSale = true;
+
+productRepository.UpdateProduct(productToUpdate);
+
+var products = productRepository.GetAllProducts();
+
+
+foreach (var product in products)
 {
-    Console.WriteLine($"{department.DepartmentID} {department.Name}");
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.StockLevel);
     Console.WriteLine();
     Console.WriteLine();
 }
+
+#endregion
